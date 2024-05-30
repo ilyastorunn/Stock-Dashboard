a7b4919d027a9c777c45b9855760b84895039d44
nihaltorunn <nihaltorunn@gmail.com>
2024-05-30 07:32:48 +0000

    Add stock tracking feature using Finnhub API
    - Integrated Finnhub API to fetch real-time stock data
    - Implemented stock tracking functionality
    - Updated UI to display stock information

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Search from "./Search";
+import ThemeIcon from "./ThemIcon";
+import { mockCompanyDetails } from "../constants/mock";
+
+const Header = ({ name }) => {
+    return <>
+        <div className="xl:px-32">
+            <h1 className="text-5xl">{mockCompanyDetails.name}</h1>
+            <Search />
+        </div>
+        <ThemeIcon/>
+    </>
+};
+
+export default Header;
